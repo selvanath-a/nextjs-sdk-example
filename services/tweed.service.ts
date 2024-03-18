a741a9ba081ba958dc683921dfc4686670e8ae94
selvanath-a <32309579+selvanath-a@users.noreply.github.com>
2024-03-18 09:56:29 +0000

    Pending changes exported from your codespace

--- services/tweed.service.ts
+++ services/tweed.service.ts
@@ -11,9 +11,9 @@ class TweedService {
     undefined;
   async initialize() {
     this._client = await TweedBackendSDK.setup({
-      apiKey: "15cVDEv7E9saty6TRjmVoel78ibLXmdc",
-      apiSecret: "7NqjB6yVfULNDx-3Y06H5T51WQN7DSPvAA7c5pOIiU-oN9kxR1a5xpaOaM4PmYn1",
-      defaultBlockchainIds: ["ethereumSepolia"],
+      apiKey: 'VRrQKLm4K0GuTnOqj2a6YtzCTWb2hRrp',
+      apiSecret: 'PLom9SJeGchA4p5U6NjMApoZL17N-QffJV08NIaTHrDU10X0ai11qtqq3Oci8ZsW',
+      defaultBlockchainIds: ['ethereumSepolia', 'polygonMumbai'],
       callbacks: {
         getNftPurchaseData: async ({ nftId }) => nftService.getById(nftId),
         getTokenPurchaseData: async ({ tokenId }) =>
